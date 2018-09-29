ee9c8cfec692f198761a34d9b8504afa05d081b4
rocamil85 <43684050+rocamil85@users.noreply.github.com>
2018-09-28 23:50:00 -0300

    last

--- JQuery/js/ajax.js
+++ JQuery/js/ajax.js
@@ -1,52 +1,66 @@
 $(document).ready(function () {
     var div1 = $("#div1");
-    
-    $("#div1").load("https://localhost:44320/Home/myAction?a=hola&b=mundo");
-    //$("#div1").load("https://reqres.in/");
-    //$("#div1").load("http://www.cubadebate.cu/");
+    form1 = $("#form1");
 
-    $.get("https://reqres.in/api/users", {page:2},
+    
+
+    //$("#div1").load("https://localhost:44320/Home/myAction?a=hola&b=mundo");
+
+
+    /* $.get("https://reqres.in/api/users", { page: 2 },
         function (response) {
-           /*for (const key in response.data) {
-               if (response.data.hasOwnProperty(key)) {
-                   const element = response.data[key];
-                   
-                   console.log(element.first_name);
-               }
-           }*/
-          
-          for(var item in response.data)
-          {              
-            console.log(response.data[item].first_name);
-            div1.append("<H2>"+response.data[item].first_name+"</H2>");
-          }
-        },
-        
+
+            //iterador mas sencillo que hay y es de javascript y no es autocompletado
+            for (var item in response.data) {
+                console.log(response.data[item].first_name);
+                div1.append("<H2>" + response.data[item].first_name + "</H2>");
+            }
+        }
     );
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ */
+    /* form1 = $("#form1");
+    $("#form1").submit(function (e) {
+        e.preventDefault();
+
+        var usuario = {
+            name: $("#input1").val(),
+            lastName: $("#input2").val()
+        };
+
+        $.post(form1.attr("action"), usuario,
+            function (usuario, textStatus, jqXHR) {
+                console.log(usuario);
+            }
+        ).done(function () {
+            alert("usuario añadido correctamente");
+        });
+    }); */
+
+    $("#form1").submit(function (e) {
+        e.preventDefault();
+
+        var usuario = {
+            name: $("#input1").val(),
+            lastName: $("#input2").val()
+        };
+
+    $.ajax({
+        type: "POST",
+        url: form1.attr("action"),
+        data: usuario,
+        dataType: "JSON",
+        success: function (response) {
+            console.log(response);
+        },
+        error: function () {
+            console.log("hubo error");
+          },
+          timeout:10000
+    });
+
+});
+
+
+
     //---
 });
