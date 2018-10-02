fe293b74872ef1354ac5163d397428b06775a333
rocamil85 <43684050+rocamil85@users.noreply.github.com>
2018-10-02 00:56:18 -0300

    introduccion de maquetado

--- JQuery/js/user interface.js
+++ JQuery/js/user interface.js
@@ -1,25 +1,42 @@
 $(document).ready(function () {
     console.log("asd");
     
+    // mover elemento
+    $(".elemento").draggable();
+    $(".drop").droppable({
+
+        drop:function () 
+        {  
+            console.log("dejaste algo dentro");
+        }
+    });
+     
+    // redimensionar elemento
+    $(".elemento").resizable();
     
     
+    //seleccionable para listas
+    //$(".lista-web").selectable();
     
+    //para ordenar listas
+    $(".lista-web").sortable();
+        
+    //Dialog
+    $("#button1").click(function () {
+        $(".efect").toggle("explode");//explode,blind,slide,drop,etc...
+        $("#dialog").dialog();
+    });
+      
+    //Tooltip
+    $(document).tooltip({
+        hide: { effect: "blind", duration: 1000 }//blind,explode,etc...
+      });
     
+    //DatePicker o calendar
+    $("#calendar").datepicker();
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //tabs
+    $("#pestannas").tabs();
     
     
     
