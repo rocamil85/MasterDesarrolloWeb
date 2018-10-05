4eb0cd854a885e06e64f9381fe2a2b1da93585c4
rocamil85 <43684050+rocamil85@users.noreply.github.com>
2018-10-05 16:38:38 -0300

    173

--- poo-typescript/js/camiseta.js
+++ poo-typescript/js/camiseta.js
@@ -1,4 +1,35 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+//Decorator
+function arranque(lanzar) {
+    return function (target) {
+        target.prototype.lanzamiento = function () {
+            alert(lanzar);
+        };
+    };
+}
+//ejemplo de un modulo interno(no me pincha revisar  bien...)
+/*module Tienda{
+    export Class Ropa{
+        constructor(titulo:string){
+            alert(titulo);
+        }
+    }
+
+    export Class Informatica{
+        constructor(titulo:string){
+            alert("Tienda de tecnologia: "+ titulo );
+        }
+    }
+}
+
+import Informatica = Tienda.Informatica;
+let cargarInformatica = new Informatica("supertienda");*/
 //clases
 var Camiseta = /** @class */ (function () {
     function Camiseta(color, modelo, marca, talla, precio) {
@@ -12,8 +43,12 @@ var Camiseta = /** @class */ (function () {
         return this.color + " " + this.marca;
     };
     ;
+    Camiseta = __decorate([
+        arranque("Ejemplo de un decorator")
+    ], Camiseta);
     return Camiseta;
 }());
 var a = new Camiseta("rojo", "adidas", "Abc", "M", 15);
 var b = a.Mix();
 console.log(b);
+a.lanzamiento();
--- poo-typescript/js/camiseta.ts
+++ poo-typescript/js/camiseta.ts
@@ -1,4 +1,32 @@
+//Decorator
+function arranque(lanzar: string) {
+    return function (target: Function) {
+        target.prototype.lanzamiento = function(): void {
+            alert(lanzar);
+        }
+    }
+}
+
+//ejemplo de un modulo interno(no me pincha revisar  bien...)
+/*module Tienda{
+    export Class Ropa{
+        constructor(titulo:string){
+            alert(titulo);
+        }
+    }
+
+    export Class Informatica{
+        constructor(titulo:string){
+            alert("Tienda de tecnologia: "+ titulo );
+        }
+    }
+}
+
+import Informatica = Tienda.Informatica;
+let cargarInformatica = new Informatica("supertienda");*/
+
 //clases
+@arranque("Ejemplo de un decorator")
 class Camiseta {
     public color: string;
     public modelo: string;
@@ -7,14 +35,14 @@ class Camiseta {
     public precio: number;
 
 
-   public constructor(color: string, modelo: string, marca: string, talla: string, precio: number) {
+    public constructor(color: string, modelo: string, marca: string, talla: string, precio: number) {
         this.color = color;
         this.modelo = modelo;
         this.marca = marca;
         this.talla = talla;
         this.precio = precio;
     }
-    public Mix():string {
+    public Mix(): string {
         return this.color + " " + this.marca;
     };
 }
@@ -23,3 +51,5 @@ class Camiseta {
 var a = new Camiseta("rojo", "adidas", "Abc", "M", 15);
 var b = a.Mix();
 console.log(b);
+
+a.lanzamiento();
